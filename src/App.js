37be3bb7704d07b44af2a4a9d7328976cf9f8393
Vens <v.kolev@alfatrex.com>
2022-01-26 16:38:26 +0100

    Style fix

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import './App.css';
-import Card from './component/Card';
+import React from "react";
+import "./App.css";
+import Card from "./component/Card";
 
 function App() {
   return (
-    <div className='Header'>
+    <div className="Header">
       <div className="App">
-      <Card bank={"Big Bank, Inc."}
-      nummer={"1234 5432 1234 3455"}
-      valide={"1234"}
-      validate={"VALID THRU"}
-      datum={"08/19"}
-      namen={"CARDHOLDER NAME"}
-      />
-      
-
+        <Card
+          bank={"Big Bank, Inc."}
+          nummer={"1234 5432 1234 3455"}
+          valide={"1234"}
+          validate1={"VALID"}
+          validate2={"THRU"}
+          datum={"08/19"}
+          namen={"CARDHOLDER NAME"}
+        />
       </div>
     </div>
   );
--- src/component/Card.js
+++ src/component/Card.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
 
-function Card({ bank, nummer, valide, validate, datum, namen }) {
+function Card({ bank, nummer, valide, validate1, validate2, datum, namen }) {
   return (
-  <div 
-className="Card"
->
-  <h2 className='Bank'>{bank}</h2>
-  <h2 className='Nummer'>{nummer}</h2>
-  <span className='Valide'>{valide}</span>
-  <span className='Validate'>{validate}</span>
-  <span className='Datum'>{datum}</span>
-  <h2 className='Namen'>{namen}</h2>
-  </div>
+    <div className="Card">
+      <h2 className="Bank">{bank}</h2>
+      <h2 className="Nummer">{nummer}</h2>
+      <span className="Valide">{valide}</span>
+      <div className="Inline">
+        <div className="Block">
+          <span className="Validate1">{validate1}</span>
+          <span className="Validate2">{validate2}</span>
+        </div>
+        <span className="Datum">{datum}</span>
+      </div>
+      <h2 className="Namen">{namen}</h2>
+    </div>
   );
 }
 
